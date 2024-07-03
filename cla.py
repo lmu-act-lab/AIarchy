6a7899e6cbbabe020dbbb8747891556640c2e773
Relectrik <viv@dluvs.com>
2024-07-02 22:20:54 -0700

    added more/cleaned methods for cla, working on expected reward next

--- cla.py
+++ cla.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import warnings
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import networkx as nx  # type: ignore
 import copy
+from typing import Callable
 from util import *
 from pgmpy.factors.discrete import TabularCPD  # type: ignore
 from pgmpy.models import BayesianNetwork  # type: ignore
@@ -19,6 +21,7 @@ class CausalLearningAgent:
         reflective_vars: set[str],
         chance_vars: set[str],
         glue_vars: set[str],
+        reward_func: Callable[[dict[str, int]], dict[str, float]],
         fixed_evidence: dict[str, int] = {},
         weights: dict[str, float] = {},
         threshold: float = 0.1,
@@ -73,6 +76,7 @@ class CausalLearningAgent:
                 {key: 1 / len(self.utility_vars) for key in self.utility_vars}
             )
         self.weight_history: dict[int, dict[str, float]] = {}
+        self.reward_func = reward_func
 
         for cpt in cpts:
             self.sampling_model.add_cpds(cpt)
@@ -86,50 +90,175 @@ class CausalLearningAgent:
 
         self.original_model: BayesianNetwork = copy.deepcopy(self.sampling_model)
 
-    def get_cpts(
-        self, vars: set[str] = set(), original: bool = False
-    ) -> list[TabularCPD]:
+    def get_cpts(self) -> list[TabularCPD]:
         """
-        Returns the cpts of the model
-
-        Args:
-            vars (set[str], optional): specific vars you'd want the cpts for. Defaults to {}.
-            original (bool, optional): whether you want to see the original cpts. Defaults to False.
+        Returns the CPDs of the sampling model.
 
         Returns:
-            list[TabularCPD]: a list of cpts pertaining to the sampling model.
+            list[TabularCPD]: List of CPDs.
         """
-        return (
-            (
-                self.original_model.get_cpds()
-                if original
-                else self.sampling_model.get_cpds()
-            )
-            if not vars
-            else [
-                (
-                    self.original_model.get_cpds(var)
-                    if original
-                    else self.sampling_model.get_cpds(var)
-                )
-                for var in vars
-            ]
-        )
-    
+        return self.sampling_model.get_cpds()
+
+    def get_original_cpts(self) -> list[TabularCPD]:
+        """
+        Returns the CPDs of the original model.
+
+        Returns:
+            list[TabularCPD]: List of CPDs.
+        """
+        return self.original_model.get_cpds()
+
+    def get_var_cpt(self, var: str) -> list[TabularCPD]:
+        """
+        Returns the CPDs of a particular variable.
+
+        Args:
+            var (str): The variable to get the CPD of.
+
+        Returns:
+            list[TabularCPD]: List of CPDs.
+        """
+        return self.sampling_model.get_cpds(var)
+
+    def get_original__var_cpt(self, var: str) -> list[TabularCPD]:
+        """
+        Returns the CPDs of a particular variable in the original model.
+
+        Args:
+            var (str): The variable to get the CPD of.
+
+        Returns:
+            list[TabularCPD]: List of CPDs.
+        """
+        return self.original_model.get_cpds(var)
+
     def display_memory(self) -> None:
+        """
+        Displays the memory of the agent.
+        """
         for time_step in self.memory:
             print(time_step)
             print("\n")
 
     def display_weights(self) -> None:
+        """
+        Displays the weights of the agent.
+        """
         for time_step, weights in self.weight_history.items():
             print(f"Iteration {time_step}:")
             print(weights)
             print("\n")
 
-    # def display_cpt(self, var: str = "") -> None:
-    #     for cpd in self.model.get_cpds():
-    #         if cpd.variable == var:
-    #             print(f"CPD of {cpd.variable}:")
-    #             print(cpd)
-    #             print("\n")
+    def display_cpts(self) -> None:
+        """
+        Displays the CPDs of the sampling model.
+        """
+        for cpd in self.sampling_model.get_cpds():
+            print(f"CPD of {cpd.variable}:")
+            print(cpd)
+            print("\n")
+
+    def display_var_cpt(self, var: str) -> None:
+        """
+        Displays the CPD of a particular variable.
+
+        Args:
+            var (str): The variable to display the CPD of.
+        """
+        for cpd in self.sampling_model.get_cpds():
+            if cpd.variable == var:
+                print(f"CPD of {cpd.variable}:")
+                print(cpd)
+                print("\n")
+
+    def display_original_cpts(self) -> None:
+        """
+        Displays the CPDs of the original model.
+        """
+        for cpd in self.original_model.get_cpds():
+            print(f"CPD of {cpd.variable}:")
+            print(cpd)
+            print("\n")
+
+    def draw_model(self):
+        """
+        Draws the structural model of the agent.
+        """
+        # Initialize the graph
+        G = nx.DiGraph()
+        G.add_edges_from(self.structural_model.edges())
+
+        # Define colors for each type of variable
+        color_map = {
+            "reflective": "blue",
+            "glue": "green",
+            "chance": "red",
+            "utility": "yellow",
+        }
+
+        # Initialize a list to hold edge colors
+        edge_colors = []
+
+        # Determine the color of each edge based on the variable type
+        for edge in G.edges():
+            source, target = edge
+            if target in self.reflective_vars:
+                edge_colors.append(color_map["reflective"])
+            elif target in self.glue_vars:
+                edge_colors.append(color_map["glue"])
+            elif target in self.chance_vars:
+                edge_colors.append(color_map["chance"])
+            elif target in self.utility_vars:
+                edge_colors.append(color_map["utility"])
+            else:
+                edge_colors.append("black")  # Default color
+
+        # Draw the graph
+        pos = nx.spring_layout(G)  # Positions for all nodes
+        nx.draw(
+            G,
+            pos,
+            edges=G.edges(),
+            edge_color=edge_colors,
+            with_labels=True,
+            node_size=2000,
+            node_color="lightgrey",
+            font_size=12,
+            font_weight="bold",
+        )
+
+        # Display the plot
+        plt.show()
+
+    def plot_memory(self) -> None:
+        """
+        Plots the perceived reward at each iteration.
+        """
+        plot_data: dict[str, list[float]] = {}
+
+        for rewards in self.memory:
+            for key, value in rewards.items():
+                if key not in plot_data:
+                    plot_data[key] = []
+                plot_data[key].append(value)
+
+        # Plot each key as a separate line
+        for key, values in plot_data.items():
+            plt.plot(values, label=key)
+
+        # Add labels and a legend
+        plt.xlabel("Iteration")
+        plt.ylabel("Perceived Reward")
+        plt.title("Perceived reward at iteration t")
+        plt.legend()
+
+        # Show the plot
+        plt.show()
+
+    def calculate_expected_reward(
+        self,
+        sample: dict[str, int],
+        rewards: dict[str, float],
+    ) -> dict[str, float]:
+        
+        pass
