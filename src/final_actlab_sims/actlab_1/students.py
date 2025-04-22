19e0bcc7a28a4d09b7047108114397cb6b11867e
Relectrik <viv@dluvs.com>
2025-04-22 15:52:46 -0700

    Creating Simulations

---
+++ src/final_actlab_sims/actlab_1/students.py
@@ -0,0 +1,20 @@
+from src.final_actlab_sims.models import students
+from src.training_environment import TrainingEnvironment
+import copy
+import logging
+import os
+
+logging.disable(logging.WARNING)
+testing_environment: TrainingEnvironment = TrainingEnvironment()
+
+
+for name, student_agent in students.items():
+    os.makedirs(f"{name}", exist_ok=True)
+    testing_environment.pre_training_visualization(student_agent, name=name, save=True)
+    student_agents = [copy.deepcopy(student_agent) for agent in range(10)]
+    testing_environment.train(10, "SA", student_agents)
+    testing_environment.post_training_visualization(student_agents, name=name, save=True)
+    testing_environment.show_cpt_changes(student_agents, name=name,  save=True)
+    testing_environment.plot_monte_carlo(student_agents, show_params=True, name=name, save=True)
+    testing_environment.plot_weighted_rewards(student_agents, show_params=True, name=name, save=True)
+    testing_environment.plot_u_hat_model_losses(student_agents, name=name, save=True)
---
+++ src/final_actlab_sims/actlab_3/base_models.py
@@ -0,0 +1,6 @@
+from src.final_actlab_sims.models import base_models
+from src.cla import CausalLearningAgent
+from src.training_environment import TrainingEnvironment
+
+testing_environment: TrainingEnvironment = TrainingEnvironment()
+
