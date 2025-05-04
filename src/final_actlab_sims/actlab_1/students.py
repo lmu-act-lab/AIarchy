7b54a7d617c60197bae3d140174b997da8aef0c4
CS Admin <csadmin@DOO217-03.lmumain.edu>
2025-05-03 17:43:43 -0700

    separating student training

--- src/final_actlab_sims/actlab_1/students.py
+++ src/final_actlab_sims/actlab_1/students.py
@@ -20,16 +20,3 @@ for name, student_agent in students.items():
     testing_environment.plot_weighted_rewards(student_agents, show_params=True, name=name, save=True)
     testing_environment.plot_u_hat_model_losses(student_agents, name=name, save=True)
     testing_environment.save_reward_csv(student_agents, name=name)
-
-for name, student_agent in students.items():
-    name += "_noise"
-    os.makedirs(f"{name}", exist_ok=True)
-    testing_environment.pre_training_visualization(student_agent, name=name, save=True)
-    student_agents = [copy.deepcopy(student_agent) for agent in range(1)]
-    testing_environment.train(1, "SA", student_agents)
-    testing_environment.post_training_visualization(student_agents, name=name, save=True)
-    testing_environment.show_cpt_changes(student_agents, name=name,  save=True)
-    testing_environment.plot_monte_carlo(student_agents, show_params=True, name=name, save=True)
-    testing_environment.plot_weighted_rewards(student_agents, show_params=True, name=name, save=True)
-    testing_environment.plot_u_hat_model_losses(student_agents, name=name, save=True)
-    testing_environment.save_reward_csv(student_agents, name=name)
